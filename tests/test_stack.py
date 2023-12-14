4783000114e7360ac840741a23be80394d725595
kirill_tarasov <kirilltaras1792@yandex.ru>
2023-12-14 21:16:26 +0700

    add code into test_stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+from src.stack import Node, Stack
+
+test_node = Node(1, 2)
+test_stack = Stack()
+
+
+def test_init_node():
+    assert test_node.data == 1
+    assert test_node.next_node == 2
+
+
+def test_init_stack():
+    assert test_stack.top is None
